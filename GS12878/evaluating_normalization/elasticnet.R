e9cbc75c9ffc0d0a79dd57958beaf554704fb8d7
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-06-12 00:55:00 -0400

    adding no log with no standardization and log with no standardization

--- GS12878/evaluating_normalization/elasticnet.R
+++ GS12878/evaluating_normalization/elasticnet.R
@@ -63,18 +63,18 @@ simple_roc <- function(labels, scores){
 
 ########################################################################################
 
-#With log transform
+#With log transform and standardization
 
 #set length of list objects that will be filled in with specificities
 #and sensitivities and aucs and variable importance
-enetlst <- list(tpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
+enetlst_ls <- list(tpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
                              ncol=bootsamps),
                 fpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
                               ncol=bootsamps),
                 auc <- numeric(bootsamps),
                 varimp <- matrix(nrow=dim(chr1data_f)[2]-1,
                                 ncol=bootsamps))
-rownames(enetlst[[4]]) <- colnames(chr1data_f)[-1]
+rownames(enetlst_ls[[4]]) <- colnames(chr1data_f)[-1]
 
 #enetlst <- list(tpr <- matrix(nrow=ceiling((length(which(logitdata_f$y==1))*2)*.3), 
 #                              ncol=bootsamps),
@@ -112,34 +112,97 @@ for(i in 1:bootsamps){
   pred.eNetModel <- as.vector(predict(eNetModel, 
                                       newdata=test, 
                                       type="prob")[,"Yes"])
-  enetlst[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,1]
-  enetlst[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,2]
-  enetlst[[3]][i] <- pROC::auc(pROC::roc(test$y, pred.eNetModel))
-  enetlst[[4]][,i] <- varImp(eNetModel)$importance[,1]
+  enetlst_ls[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,1]
+  enetlst_ls[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,2]
+  enetlst_ls[[3]][i] <- pROC::auc(pROC::roc(test$y, pred.eNetModel))
+  enetlst_ls[[4]][,i] <- varImp(eNetModel)$importance[,1]
   
 }
 
-mean(enetlst[[3]])
+mean(enetlst_ls[[3]])
 
-saveRDS(enetlst, "enetlst.rds")
+saveRDS(enetlst_ls, "enetlst_ls.rds")
 
-########################################################################################
+#################################################################################
 
-#Without log transform
+#With log transform and no standardization
+
+#set length of list objects that will be filled in with specificities
+#and sensitivities and aucs and variable importance
+enetlst_lns <- list(tpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
+                                 ncol=bootsamps),
+                   fpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
+                                 ncol=bootsamps),
+                   auc <- numeric(bootsamps),
+                   varimp <- matrix(nrow=dim(chr1data_f)[2]-1,
+                                    ncol=bootsamps))
+rownames(enetlst_lns[[4]]) <- colnames(chr1data_f)[-1]
+
+#enetlst <- list(tpr <- matrix(nrow=ceiling((length(which(logitdata_f$y==1))*2)*.3), 
+#                              ncol=bootsamps),
+#                fpr <- matrix(nrow=ceiling((length(which(logitdata_f$y==1))*2)*.3), 
+#                              ncol=bootsamps),
+#                auc <- numeric(bootsamps),
+#                varimp <- matrix(nrow=dim(logitdata_f)[2]-1,
+#                                 ncol=bootsamps))
+#rownames(enetlst[[4]]) <- colnames(logitdata_f)[-1]
+
+
+
+for(i in 1:bootsamps){
+  set.seed(7215)
+  #combining the two classes to create balanced data
+  data <- rbind.data.frame(chr1data_f[which(chr1data_f$y=="Yes"),],
+                           chr1data_f[sampids[,i],])
+  
+  #data <- rbind.data.frame(logitdata_f[which(logitdata_f$y==1),],
+  #                         logitdata_f[sampids[,i],])
+  
+  inTrainingSet <- sample(length(data$y),floor(length(data$y)*.7))
+  #inTrainingSet <- createDataPartition(data$y,p=.7,list=FALSE)
+  train <- data[inTrainingSet,]
+  test <- data[-inTrainingSet,]
+  
+  #ENET Model
+  eNetModel <- train(y ~ ., data=train, 
+                     method = "glmnet", 
+                     metric="ROC", 
+                     trControl = fitControl, 
+                     family="binomial", 
+                     tuneLength=5,
+                     standardize=FALSE)
+  pred.eNetModel <- as.vector(predict(eNetModel, 
+                                      newdata=test, 
+                                      type="prob")[,"Yes"])
+  enetlst_lns[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,1]
+  enetlst_lns[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,2]
+  enetlst_lns[[3]][i] <- pROC::auc(pROC::roc(test$y, pred.eNetModel))
+  enetlst_lns[[4]][,i] <- varImp(eNetModel)$importance[,1]
+  
+}
+
+mean(enetlst_lns[[3]])
+
+saveRDS(enetlst_lns, "enetlst_lns.rds")
+
+
+#################################################################################
+
+#Without log transform and standardization
 
 cols <- c(grep("dist",colnames(chr1data_f)))
 chr1data_f[,cols] <- apply(chr1data_f[,cols], 2, function(x){2^x})
 
 #set length of list objects that will be filled in with specificities
 #and sensitivities and aucs and variable importance
-enetlst_nl <- list(tpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
+enetlst_nls <- list(tpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
                              ncol=bootsamps),
                 fpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
                               ncol=bootsamps),
                 auc <- numeric(bootsamps),
                 varimp <- matrix(nrow=dim(chr1data_f)[2]-1,
                                 ncol=bootsamps))
-rownames(enetlst_nl[[4]]) <- colnames(chr1data_f)[-1]
+rownames(enetlst_nls[[4]]) <- colnames(chr1data_f)[-1]
 
 #enetlst <- list(tpr <- matrix(nrow=ceiling((length(which(logitdata_f$y==1))*2)*.3), 
 #                              ncol=bootsamps),
@@ -177,28 +240,95 @@ for(i in 1:bootsamps){
   pred.eNetModel <- as.vector(predict(eNetModel, 
                                       newdata=test, 
                                       type="prob")[,"Yes"])
-  enetlst_nl[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,1]
-  enetlst_nl[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,2]
-  enetlst_nl[[3]][i] <- pROC::auc(pROC::roc(test$y, pred.eNetModel))
-  enetlst_nl[[4]][,i] <- varImp(eNetModel)$importance[,1]
+  enetlst_nls[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,1]
+  enetlst_nls[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,2]
+  enetlst_nls[[3]][i] <- pROC::auc(pROC::roc(test$y, pred.eNetModel))
+  enetlst_nls[[4]][,i] <- varImp(eNetModel)$importance[,1]
   
 }
 
-mean(enetlst_nl[[3]])
+mean(enetlst_nls[[3]])
 
-saveRDS(enetlst_nl, "enetlst_nl.rds")
+saveRDS(enetlst_nls, "enetlst_nls.rds")
+
+####################################################################
+
+#Without log transform and no standardization
+
+enetlst_nlns <- list(tpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
+                                  ncol=bootsamps),
+                    fpr <- matrix(nrow=ceiling((length(which(chr1data_f$y=="Yes"))*2)*.3), 
+                                  ncol=bootsamps),
+                    auc <- numeric(bootsamps),
+                    varimp <- matrix(nrow=dim(chr1data_f)[2]-1,
+                                     ncol=bootsamps))
+rownames(enetlst_nlns[[4]]) <- colnames(chr1data_f)[-1]
+
+#enetlst <- list(tpr <- matrix(nrow=ceiling((length(which(logitdata_f$y==1))*2)*.3), 
+#                              ncol=bootsamps),
+#                fpr <- matrix(nrow=ceiling((length(which(logitdata_f$y==1))*2)*.3), 
+#                              ncol=bootsamps),
+#                auc <- numeric(bootsamps),
+#                varimp <- matrix(nrow=dim(logitdata_f)[2]-1,
+#                                 ncol=bootsamps))
+#rownames(enetlst[[4]]) <- colnames(logitdata_f)[-1]
+
+
+
+for(i in 1:bootsamps){
+  set.seed(7215)
+  #combining the two classes to create balanced data
+  data <- rbind.data.frame(chr1data_f[which(chr1data_f$y=="Yes"),],
+                           chr1data_f[sampids[,i],])
+  
+  #data <- rbind.data.frame(logitdata_f[which(logitdata_f$y==1),],
+  #                         logitdata_f[sampids[,i],])
+  
+  inTrainingSet <- sample(length(data$y),floor(length(data$y)*.7))
+  #inTrainingSet <- createDataPartition(data$y,p=.7,list=FALSE)
+  train <- data[inTrainingSet,]
+  test <- data[-inTrainingSet,]
+  
+  #ENET Model
+  eNetModel <- train(y ~ ., data=train, 
+                     method = "glmnet", 
+                     metric="ROC", 
+                     trControl = fitControl, 
+                     family="binomial", 
+                     tuneLength=5,
+                     standardize=FALSE)
+  pred.eNetModel <- as.vector(predict(eNetModel, 
+                                      newdata=test, 
+                                      type="prob")[,"Yes"])
+  enetlst_nlns[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,1]
+  enetlst_nlns[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.eNetModel)[,2]
+  enetlst_nlns[[3]][i] <- pROC::auc(pROC::roc(test$y, pred.eNetModel))
+  enetlst_nlns[[4]][,i] <- varImp(eNetModel)$importance[,1]
+  
+}
+
+mean(enetlst_nlns[[3]])
+
+saveRDS(enetlst_nlns, "enetlst_nlns.rds")
+
 
 #####################################################################
 
 #plotting performance
 
 
-
-test.auc <- data.frame(Normalization=c("With", "Without"),auc=c(mean(enetlst_wl[[3]]),mean(enetlst_nl[[3]])))
+test.auc <- data.frame(Normalization=c("Log/Standardization", 
+                                       "Log/No Standardization",
+                                       "No Log/Standardization",
+                                       "No Log/No Standardization"),
+                       auc=c(mean(enetlst_ls[[3]]),
+                             mean(enetlst_lns[[3]]),
+                             mean(enetlst_nls[[3]]),
+                             mean(enetlst_nlns[[3]])))
 
 test.auc <- test.auc[order(test.auc$auc, decreasing=TRUE),]
 
-#test.auc$model <- factor(test.auc$model, levels=test.auc$model)
+test.auc$model <- factor(test.auc$Normalization, levels=test.auc$Normalization)
 
 test.auc
 
