1e522ac3c67683024f12918304360725c969d4f5
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-06-12 03:54:12 -0400

    edits

--- GS12878/05model_filtering.R
+++ GS12878/05model_filtering.R
@@ -17,50 +17,59 @@ library(ggplot2)
 #setwd("/home/stilianoudakisc/TAD_data_analysis/Rdata")
 setwd("C:/Users/Spiro Stilianoudakis/Documents/TAD_data/RData")
 
-gs12878 <- readRDS("gs12878.rds")
+gm12878 <- readRDS("gm12878.rds")
 
 ##################################################################
 
 #Full Data
 
 #Remove CHR variable
-gs12878_f <- gs12878[,-which(colnames(gs12878)=="CHR")]
+gm12878_f <- gm12878[,-which(colnames(gm12878)=="CHR")]
 
 #Taking log2 transform of continous data
-cols <- c(grep("dist",colnames(gs12878_f)))
-gs12878_f[,cols] <- apply(gs12878_f[,cols], 2, function(x){log(x + 1, base=2)})
+cols <- c(grep("dist",colnames(gm12878_f)))
+gm12878_f[,cols] <- apply(gm12878_f[,cols], 2, function(x){log(x + 1, base=2)})
 
 #Changing binary variables to factors
-cols <- c(grep("dist",colnames(gs12878_f), invert = TRUE))
-gs12878_f[,cols] <- lapply(gs12878_f[,cols], factor)
+cols <- c(grep("score",colnames(gm12878_f), invert = TRUE),
+          grep("dist",colnames(gm12878_f), invert = TRUE))
+gm12878_f[,cols] <- lapply(gm12878_f[,cols], factor)
 
 #Changing levels of response (y) to yes no
-levels(gs12878_f$y) <- c("No", "Yes")
+levels(gm12878_f$y) <- c("No", "Yes")
 
 #Removing zero variance predictors
-nzv <- nearZeroVar(gs12878_f[,-1], saveMetrics= TRUE)
+nzv <- nearZeroVar(gm12878_f[,-1], saveMetrics= TRUE)
 nzvar <- rownames(nzv[nzv$nzv,])
-gs12878_f <- gs12878_f[, -which(colnames(gs12878_f) %in% nzvar)]
+gm12878_f <- gm12878_f[, -which(colnames(gm12878_f) %in% nzvar)]
 
 
-saveRDS(gs12878_f, "gs12878_f.rds")
+saveRDS(gm12878_f, "gm12878_f.rds")
 
 ##
 
 #Chromosome 1
 
-chr1_gs12878 <- gs12878[which(gs12878$CHR=="chr1"),]
+chr1_gm12878 <- gm12878[which(gm12878$CHR=="chr1"),]
 
-cols <- c(grep("dist",colnames(chr1_gs12878)))
-chr1_gs12878[,cols] <- apply(chr1_gs12878[,cols], 2, function(x){log(x + 1, base=2)})
+#Taking log2 transform of continous data
+cols <- c(grep("dist",colnames(chr1_gm12878)))
+chr1_gm12878[,cols] <- apply(chr1_gm12878[,cols], 2, function(x){log(x + 1, base=2)})
 
-nzv <- nearZeroVar(chr1_gs12878[,-1], saveMetrics= TRUE)
-nzvar <- rownames(nzv[nzv$nzv,])
+#Changing binary variables to factors
+cols <- c(intersect(grep("score",colnames(chr1_gm12878), invert = TRUE),
+                    grep("dist",colnames(chr1_gm12878), invert = TRUE)))
+chr1_gm12878[,cols] <- lapply(chr1_gm12878[,cols], factor)
+
+#Changing levels of response (y) to yes no
+levels(chr1_gm12878$y) <- c("No", "Yes")
 
 #Removing zero variance predictors
-chr1_gs12878_f <- chr1_gs12878[, -which(colnames(chr1_gs12878) %in% nzvar)]
+nzv <- nearZeroVar(chr1_gm12878[,-1], saveMetrics= TRUE)
+nzvar <- rownames(nzv[nzv$nzv,])
+chr1_gm12878_f <- chr1_gm12878[, -which(colnames(chr1_gm12878) %in% nzvar)]
 
 
-saveRDS(chr1_gs12878_f, "chr1_gs12878_f.rds")
+saveRDS(chr1_gm12878_f, "chr1_gm12878_f.rds")
 
 
