8e7b5f360816c78db224ddc0cdd7a2579ab474c3
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-06-25 09:31:55 -0400

    updates

--- GM12878/evaluating_variable_reduction/Boruta/reducing_variables.R
+++ GM12878/evaluating_variable_reduction/Boruta/reducing_variables.R
@@ -39,6 +39,7 @@ print(boruta_chr1)
 
 saveRDS(boruta_chr1, "boruta_chr1.rds")
 
+par(mar=c(7, 4, 2, 2))
 plot(boruta_chr1, xlab = "", xaxt = "n")
 
 lz<-lapply(1:ncol(boruta_chr1$ImpHistory),function(i)
@@ -46,8 +47,16 @@ lz<-lapply(1:ncol(boruta_chr1$ImpHistory),function(i)
 
 names(lz) <- colnames(boruta_chr1$ImpHistory)
 Labels <- sort(sapply(lz,median))
-axis(side = 1,las=2,labels = names(Labels),
+Labels <- names(Labels)
+Labels[grep("Gm12878_", Labels)] <- gsub("Gm12878_","",Labels[grep("Gm12878_", Labels)])
+Labels[which(Labels=="mobile_element_insertion_dist")] <- "MEI_dist"
+Labels[which(Labels=="tandem_duplication_dist")] <- "TD_dist"
+Labels[which(Labels=="novel_sequence_insertion_dist")] <- "NSI_dist"
+Labels[which(Labels=="sequence_alteration_dist")] <- "SA_dist"
+axis(side = 1,las=2,labels = Labels,
      at = 1:ncol(boruta_chr1$ImpHistory), cex.axis = 0.7)
+
+par(mar=c(4, 4, 2, 2))
 
 #final.boruta <- TentativeRoughFix(boruta_chr1)
 #print(final.boruta)
@@ -57,3 +66,46 @@ feats <- getSelectedAttributes(boruta_chr1, withTentative = T)
 boruta_chr1_gm12878 <- chr1_gm12878_f[,c("y",feats)]
 
 saveRDS(boruta_chr1_gm12878, "boruta_chr1_gm12878.rds")
+
+##################################################################
+
+#randomly sample to reduce dataset
+set.seed(123)
+zclass <- which(chr1_gm12878_f$y=="No")
+samps <- sample(which(chr1_gm12878_f$y=="No"),length(which(chr1_gm12878_f$y=="Yes")))
+chr1_gm12878_f_r <- rbind.data.frame(chr1_gm12878_f[samps,],
+                                     chr1_gm12878_f[which(chr1_gm12878_f$y=="Yes"),])
+
+boruta_chr1_r <- Boruta(y ~ ., data=chr1_gm12878_f_r,
+                        doTrace=2,
+                        getImp=getImpFerns)
+print(boruta_chr1_r)
+
+saveRDS(boruta_chr1_r, "boruta_chr1_r.rds")
+
+par(mar=c(7, 4, 2, 2))
+plot(boruta_chr1_r, xlab = "", xaxt = "n")
+
+lz<-lapply(1:ncol(boruta_chr1_r$ImpHistory),function(i)
+  boruta_chr1_r$ImpHistory[is.finite(boruta_chr1_r$ImpHistory[,i]),i])
+
+names(lz) <- colnames(boruta_chr1_r$ImpHistory)
+Labels <- sort(sapply(lz,median))
+Labels <- names(Labels)
+Labels[grep("Gm12878_", Labels)] <- gsub("Gm12878_","",Labels[grep("Gm12878_", Labels)])
+Labels[which(Labels=="mobile_element_insertion_dist")] <- "MEI_dist"
+Labels[which(Labels=="tandem_duplication_dist")] <- "TD_dist"
+Labels[which(Labels=="novel_sequence_insertion_dist")] <- "NSI_dist"
+Labels[which(Labels=="sequence_alteration_dist")] <- "SA_dist"
+axis(side = 1,las=2,labels = Labels,
+     at = 1:ncol(boruta_chr1_r$ImpHistory), cex.axis = 0.7)
+
+par(mar=c(4, 4, 2, 2))
+
+#final.boruta <- TentativeRoughFix(boruta_chr1)
+#print(final.boruta)
+
+feats_r <- getSelectedAttributes(boruta_chr1_r, withTentative = T)
+
+boruta_chr1_gm12878_r <- chr1_gm12878_f_r[,c("y",feats)]
+
--- GM12878/evaluating_variable_reduction/variable_selection/fwrd_bwrd_both.R
+++ GM12878/evaluating_variable_reduction/variable_selection/fwrd_bwrd_both.R
@@ -8,7 +8,7 @@ library(gbm)
 library(pROC)
 library(plyr)
 library(dplyr)
-#library(DMwR)
+library(DMwR)
 library(gridExtra)
 library(ggplot2)
 library(leaps)
@@ -19,19 +19,25 @@ setwd("/home/stilianoudakisc/TAD_data_analysis/comparing_normalization/")
 #Chromosome 1
 chr1_gm12878_f <- readRDS("chr1_gm12878_f.rds")
 
-#randomly sample to reduce dataset
-set.seed(123)
-chr1_gm12878_f <- chr1_gm12878_f[sample(nrow(chr1_gm12878_f),10000),c(1,sample(2:66,20))]
+#center and scaling data to avoid using intercept term
+cols <- names(Filter(is.numeric, chr1_gm12878_f))
+chr1_gm12878_f[,cols] <- scale(chr1_gm12878_f[,cols], center = TRUE, scale = TRUE)
+
+cols <- grep("dist", colnames(chr1_gm12878_f), invert=T)[-1]
+chr1_gm12878_f[,cols] <- apply(chr1_gm12878_f[,cols],2,as.numeric)
+
+##########################################################################
+
+#Use smote to create balanced data 
+chr1_gm12878_f_smote <- SMOTE(y ~ ., 
+                              data=chr1_gm12878_f, 
+                              perc.over = 100, 
+                              perc.under = 200)
 
 #Full data
 #gm12878_f <- readRDS("gm12878_f.rds")
 
 # Performing stepwise selection
-
-#center and scaling data to avoid using intercept term
-cols <- names(Filter(is.numeric, chr1_gm12878_f))
-chr1_gm12878_f[,cols] <- scale(chr1_gm12878_f[,cols], center = TRUE, scale = TRUE)
-
 
 
 #Using cross validation (10 fold)
@@ -39,15 +45,15 @@ chr1_gm12878_f[,cols] <- scale(chr1_gm12878_f[,cols], center = TRUE, scale = TRU
 #forward
 k = 10
 set.seed(789)
-folds = sample(1:k,nrow(chr1_gm12878_f), replace=TRUE)
-cv.preds.fwd=matrix(NA, nrow=ncol(chr1_gm12878_f)-1,ncol=k)
+folds = sample(1:k,nrow(chr1_gm12878_f_smote), replace=TRUE)
+cv.preds.fwd=matrix(NA, nrow=ncol(chr1_gm12878_f_smote)-1,ncol=k)
 auc.model.fwd <- numeric(k)
 
 for(j in 1:k){
   #null model
-  glm.null <- glm(y ~ 1, data = chr1_gm12878_f[folds!=j,], family = binomial)
+  glm.null <- glm(y ~ 1, data = chr1_gm12878_f_smote[folds!=j,], family = binomial)
   #full model
-  glm.full <- glm(y ~ ., data = chr1_gm12878_f[folds!=j,], family = binomial)
+  glm.full <- glm(y ~ ., data = chr1_gm12878_f_smote[folds!=j,], family = binomial)
   
   best.fit.fwd = step(glm.null,
                       scope=list(lower=formula(glm.null),
@@ -55,40 +61,47 @@ for(j in 1:k){
                       direction="forward",
                       trace=0)
   
-    numpreds <- length(names(best.fit.fwd$coefficients)[-1])
-    cv.preds.fwd[(1:numpreds),j] <- names(best.fit.fwd$coefficients)[-1]
-    cols <- names(best.fit.fwd$model)
-    model <- glm(y ~ . , data = chr1_gm12878_f[folds==j,cols], family = binomial)
-    pred.model <- predict(model, newdata=chr1_gm12878_f[folds==j,cols], type="response")
-    roc.model <- roc(chr1_gm12878_f[folds==j,"y"], pred.model)
-    auc.model.fwd[j] <- pROC::auc(roc.model)
-    
+  numpreds <- length(names(best.fit.fwd$coefficients)[-1])
+  cv.preds.fwd[(1:numpreds),j] <- names(best.fit.fwd$coefficients)[-1]
+  cols <- names(best.fit.fwd$model)
+  model <- glm(y ~ . , data = chr1_gm12878_f_smote[folds==j,cols], family = binomial)
+  pred.model <- predict(model, newdata=chr1_gm12878_f_smote[folds==j,cols], type="response")
+  roc.model <- roc(chr1_gm12878_f_smote[folds==j,"y"], pred.model)
+  auc.model.fwd[j] <- pROC::auc(roc.model)
+  
 }
 
-saveRDS(cv.preds.fwd, "cv.preds.fwd.rds")
-saveRDS(auc.model.fwd, "auc.model.fwd.rds")
+auc.model.fwd
+index <- order(auc.model.fwd,decreasing = T)[1]
+index <- as.character(na.omit(cv.preds.fwd[,index]))
+
+chr1_gm12878_f <- readRDS("chr1_gm12878_f.rds")
+forward_data_chr1 <- chr1_gm12878_f[,c("y",index)]
+
+saveRDS(forward_data_chr1, "forward_data_chr1")
+
 
 #backward
 k = 10
 set.seed(789)
-folds = sample(1:k,nrow(chr1_gm12878_f), replace=TRUE)
-cv.preds.bwd=matrix(NA, k, ncol(chr1_gm12878_f)-1)
+folds = sample(1:k,nrow(chr1_gm12878_f_smote), replace=TRUE)
+cv.preds.bwd=matrix(NA, k, ncol(chr1_gm12878_f_smote)-1)
 auc.model.bwd <- numeric(k)
 
 for(j in 1:k){
   #null model
-  glm.null <- glm(y ~ 1, data = chr1_gm12878_f[folds!=j,], family = binomial)
+  glm.null <- glm(y ~ 1, data = chr1_gm12878_f_smote[folds!=j,], family = binomial)
   #full model
-  #glm.full <- glm(y ~ ., data = chr1_gm12878_f[folds!=j,], family = binomial)
+  glm.full <- glm(y ~ ., data = chr1_gm12878_f_smote[folds!=j,], family = binomial)
   
   best.fit.bwd = step(glm.full, trace=0)
   
   numpreds <- length(names(best.fit.bwd$coefficients)[-1])
   cv.preds.bwd[(1:numpreds),j] <- names(best.fit.bwd$coefficients)[-1]
   cols <- names(best.fit.bwd$model)
-  model <- glm(y ~ . , data = chr1_gm12878_f[folds==j,cols], family = binomial)
-  pred.model <- predict(model, newdata=chr1_gm12878_f[folds==j,cols], type="response")
-  roc.model <- roc(chr1_gm12878_f[folds==j,"y"], pred.model)
+  model <- glm(y ~ . , data = chr1_gm12878_f_smote[folds==j,cols], family = binomial)
+  pred.model <- predict(model, newdata=chr1_gm12878_f_smote[folds==j,cols], type="response")
+  roc.model <- roc(chr1_gm12878_f_smote[folds==j,"y"], pred.model)
   auc.model.bwd[j] <- pROC::auc(roc.model)
   
 }
@@ -111,10 +124,10 @@ for(j in 1:k){
   glm.full <- glm(y ~ ., data = chr1_gm12878_f[folds!=j,], family = binomial)
   
   best.fit.both = step(glm.null, 
-                      list(lower=formula(glm.null),
-                           upper=formula(glm.full)),
-                      direction="both",
-                      trace=0)
+                       list(lower=formula(glm.null),
+                            upper=formula(glm.full)),
+                       direction="both",
+                       trace=0)
   
   numpreds <- length(names(best.fit.both$coefficients)[-1])
   cv.preds.both[(1:numpreds),j] <- names(best.fit.bwd$coefficients)[-1]
@@ -128,4 +141,9 @@ for(j in 1:k){
 
 saveRDS(cv.preds.both, "cv.preds.both.rds")
 saveRDS(auc.model.both, "auc.model.both.rds")
+
+##########################################################################
+
+#Randomly sampling from the majority class to create balanced data
+
 
