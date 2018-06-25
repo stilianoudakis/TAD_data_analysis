99a4178b3878747f374280ed8bc0cc1c16adf828
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-06-25 11:59:12 -0400

    added svm code

--- misc.R
+++ misc.R
@@ -206,3 +206,25 @@ mcols(partial1)$closest_dist <- 0
 mcols(partial2)$left_dist <- 0
 mcols(partial2)$right_dist <- 0
 mcols(partial2)$closest_dist <- 0
+
+
+
+grid_radial <- expand.grid(sigma = c(0,0.01, 0.02, 0.025, 0.03, 0.04,
+                                     0.05, 0.06, 0.07,0.08, 0.09, 0.1, 0.25, 0.5, 0.75,0.9),
+                           C = c(0,0.01, 0.05, 0.1, 0.25, 0.5, 0.75,
+                                 1, 1.5, 2,5))
+svmModel <- train(y ~., data = train, 
+                  method = "svmRadial",
+                  trControl=fitControl,
+                  metric="ROC",
+                  tuneGrid = grid_radial)
+pred.svmModel <- as.vector(predict(svmModel, 
+                                   newdata=test, 
+                                   type="prob")[,"Yes"])
+roc.svmModel <- pROC::roc(test$y, pred.svmModel)
+auclst[[1]][i] <- pROC::auc(roc.svmModel)
+#svm varimp
+performlst[[1]][,i] <- varImp(svmModel)$importance[,1]
+
+}
+
