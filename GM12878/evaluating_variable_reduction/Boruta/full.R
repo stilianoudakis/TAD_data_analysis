ee3d44bbab9315de75bdabe359c96beea3483061
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-06-26 08:12:19 -0400

    added barplots

--- GM12878/evaluating_variable_reduction/Boruta/full.R
+++ GM12878/evaluating_variable_reduction/Boruta/full.R
@@ -38,3 +38,22 @@ boruta_chr1_gm12878 <- chr1_gm12878_f[,c("y",feats)]
 saveRDS(boruta_chr1_gm12878, "boruta_chr1_gm12878.rds")
 
 
+setwd("C:/Users/Spiro Stilianoudakis/Documents/TAD_data/RData")
+
+boruta_chr1 <- readRDS("boruta_chr1.rds")
+
+par(mar=c(7,4,2,2))
+plot(boruta_chr1, xlab = "", xaxt = "n")
+lz<-lapply(1:ncol(boruta_chr1$ImpHistory),function(i)
+  boruta_chr1$ImpHistory[is.finite(boruta_chr1$ImpHistory[,i]),i])
+names(lz) <- colnames(boruta_chr1$ImpHistory)
+Labels <- sort(sapply(lz,median))
+Labels <- names(Labels)
+Labels[grep("Gm12878_", Labels)] <- gsub("Gm12878_","",Labels[grep("Gm12878_", Labels)])
+Labels[which(Labels=="novel_sequence_insertion_dist")] <- "NSI_dist"
+Labels[which(Labels=="sequence_alteration_dist")] <- "SA_dist"
+Labels[which(Labels=="tandem_duplication_dist")] <- "TD_dist"
+Labels[which(Labels=="mobile_element_insertion_dist")] <- "MEI_dist"
+axis(side = 1,las=2,labels = Labels,
+       at = 1:ncol(boruta_chr1$ImpHistory), cex.axis = 0.7)
+par(mar=c(4,4,2,2))
--- GM12878/evaluating_variable_reduction/Boruta/reduced.R
+++ GM12878/evaluating_variable_reduction/Boruta/reduced.R
@@ -40,3 +40,26 @@ feats_r <- getSelectedAttributes(boruta_chr1_r, withTentative = T)
 boruta_chr1_gm12878_r <- chr1_gm12878_f[,c("y",feats_r)]
 
 saveRDS(boruta_chr1_gm12878_r, "boruta_chr1_gm12878_r.rds")
+
+
+setwd("C:/Users/Spiro Stilianoudakis/Documents/TAD_data/RData")
+
+boruta_chr1_r <- readRDS("boruta_chr1_r.rds")
+
+par(mar=c(7,4,2,2))
+plot(boruta_chr1_r, xlab = "", xaxt = "n")
+lz<-lapply(1:ncol(boruta_chr1_r$ImpHistory),function(i)
+  boruta_chr1_r$ImpHistory[is.finite(boruta_chr1_r$ImpHistory[,i]),i])
+names(lz) <- colnames(boruta_chr1_r$ImpHistory)
+Labels <- sort(sapply(lz,median))
+Labels <- names(Labels)
+Labels[grep("Gm12878_", Labels)] <- gsub("Gm12878_","",Labels[grep("Gm12878_", Labels)])
+Labels[which(Labels=="novel_sequence_insertion_dist")] <- "NSI_dist"
+Labels[which(Labels=="sequence_alteration_dist")] <- "SA_dist"
+Labels[which(Labels=="tandem_duplication_dist")] <- "TD_dist"
+Labels[which(Labels=="mobile_element_insertion_dist")] <- "MEI_dist"
+axis(side = 1,las=2,labels = Labels,
+     at = 1:ncol(boruta_chr1_r$ImpHistory), cex.axis = 0.7)
+par(mar=c(4,4,2,2))
+
+
