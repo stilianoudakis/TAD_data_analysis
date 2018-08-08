533775dab3a30ef48cf52b55d546b936ee91f9b3
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-08-07 23:49:26 -0400

    edits

--- GM12878/07model_performance.R
+++ GM12878/07model_performance.R
@@ -1,7 +1,27 @@
 #Model Performance
 
 #plotting performance
+
+#mourad model
+setwd("C:/Users/Spiro Stilianoudakis/Documents/TAD_data/RData/GM12878/mourad_model")
+
+binMat.mati <- readRDS("binMat.mati.rds")
+Analysisi <- readRDS("Analysisi.rds")
+
+prob=predict(Analysisi,type=c("response"))
+binMat.mati$prob=prob
+
+simple_roc <- function(labels, scores){
+  labels <- labels[order(scores, decreasing=TRUE)]
+  data.frame(TPR=cumsum(labels)/sum(labels), FPR=cumsum(!labels)/sum(!labels), labels)
+}
+prob=predict(Analysisi,type=c("response"))
+head(prob)
+roc <- simple_roc(binMat.mati$Border, binMat.mati$prob)
+head(roc)
+
 auc.glmModel <- 0.7884
+
 test.auc <- data.frame(model=c("GLM","ElasticNet","RForest","GBM"),
                        auc=c(auc.glmModel,
                              mean(auclst[[1]]), 
